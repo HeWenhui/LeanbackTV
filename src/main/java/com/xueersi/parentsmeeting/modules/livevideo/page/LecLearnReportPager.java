e5d430bc46e3813cd860a1c6d398cc7c9e5c4323
linyuqiang <917866068@qq.com>
2018-01-10 14:06:31 +0800

    讲座学习反馈时间

--- src/main/java/com/xueersi/parentsmeeting/modules/livevideo/page/LecLearnReportPager.java
+++ src/main/java/com/xueersi/parentsmeeting/modules/livevideo/page/LecLearnReportPager.java
@@ -88,7 +88,12 @@ public class LecLearnReportPager extends BasePager {
         });
         LearnReportEntity.ReportEntity stu = reportEntity.getStu();
         int time = stu.getTime() / 60;
-        tv_livelec_dialog_xxsc_text.setText(time + "分钟");
+        int hour = time / 60;
+        if (hour == 0) {
+            tv_livelec_dialog_xxsc_text.setText(time % 60 + "分钟");
+        } else {
+            tv_livelec_dialog_xxsc_text.setText(hour + "小时" + time % 60 + "分钟");
+        }
         tv_livelec_dialog_zhengquelv_text.setText(stu.getRate());
         tv_livelec_dialog_zhengquelv_text2.setText(stu.getAverageRate());
         tv_livelec_dialog_paiming_text.setText("" + stu.getRank());
