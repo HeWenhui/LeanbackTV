ce7ffcf344686a0d092dedee50da4915108577b1
linyuqiang <917866068@qq.com>
2018-01-17 15:10:19 +0800

    文科，理科直播回放appkey

--- src/main/java/com/xueersi/parentsmeeting/modules/livevideo/activity/LivePlayBackVideoActivity.java
+++ src/main/java/com/xueersi/parentsmeeting/modules/livevideo/activity/LivePlayBackVideoActivity.java
@@ -186,11 +186,6 @@ public class LivePlayBackVideoActivity extends VideoActivity implements LivePlay
     protected void onVideoCreate(Bundle savedInstanceState) {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onVideoCreate(savedInstanceState);
-        if (AppConfig.DEBUG) {
-            UmsConstants.mAppKeyData.put(UmsConstants.LIVE_APP_ID_BACK, "cea2729baa8bea0791846300989459aa");
-        } else {
-            UmsConstants.mAppKeyData.put(UmsConstants.LIVE_APP_ID_BACK, "0ce62906d89f19d7ec2b1cae9a2fed43");
-        }
         times++;
         createTime = System.currentTimeMillis();
         AppBll.getInstance().registerAppEvent(this);
@@ -322,13 +317,13 @@ public class LivePlayBackVideoActivity extends VideoActivity implements LivePlay
         mVideoType = MobEnumUtil.VIDEO_LIVEPLAYBACK;
         where = getIntent().getStringExtra("where");
         isArts = getIntent().getIntExtra("isArts", 0);
-        if (isArts == 1) {
-            appID = UmsConstants.ARTS_APP_ID;
-            LiveVideoConfig.IS_SCIENCE = false;
-        } else {
-            LiveVideoConfig.IS_SCIENCE = true;
-            appID = UmsConstants.LIVE_APP_ID;
-        }
+//        if (isArts == 1) {
+//            appID = UmsConstants.ARTS_APP_ID;
+//            LiveVideoConfig.IS_SCIENCE = false;
+//        } else {
+//            LiveVideoConfig.IS_SCIENCE = true;
+//            appID = UmsConstants.LIVE_APP_ID;
+//        }
         // 如果加载不出来
         if (tvLoadingContent != null) {
             tvLoadingContent.setText("正在获取视频资源，请稍候");
