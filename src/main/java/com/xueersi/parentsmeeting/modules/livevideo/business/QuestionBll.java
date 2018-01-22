fc961c129c1b0e4008e46802d6d9cda70105dfaf
linyuqiang <917866068@qq.com>
2018-01-22 15:13:05 +0800

    语音答题日志

--- src/main/java/com/xueersi/parentsmeeting/modules/livevideo/business/QuestionBll.java
+++ src/main/java/com/xueersi/parentsmeeting/modules/livevideo/business/QuestionBll.java
@@ -417,8 +417,10 @@ public class QuestionBll implements QuestionAction, Handler.Callback, SpeechEval
                                 .getvQuestionID())) {
                             return;
                         }
-                        mAnswerRankBll.showRankList(new ArrayList<RankUserEntity>());
-                        mLiveBll.sendRankMessage(XESCODE.RANK_STU_RECONNECT_MESSAGE);
+                        if(mAnswerRankBll!=null) {
+                            mAnswerRankBll.showRankList(new ArrayList<RankUserEntity>());
+                            mLiveBll.sendRankMessage(XESCODE.RANK_STU_RECONNECT_MESSAGE);
+                        }
                         hasQuestion = true;
                     }
                 });
@@ -1670,18 +1672,30 @@ public class QuestionBll implements QuestionAction, Handler.Callback, SpeechEval
                 super.onPmFailure(error, msg);
                 //showFullMarkList(type, new ArrayList<FullMarkListEntity>(), delayTime);
                 mAnswerRankBll.hideRankList();
+                if (mAnswerRankBll != null) {
+                    mAnswerRankBll.hideRankList();
+                }
             }
 
             @Override
             public void onPmError(ResponseEntity responseEntity) {
                 super.onPmError(responseEntity);
                 showFullMarkList(type, new ArrayList<FullMarkListEntity>(), delayTime);
+                if (mAnswerRankBll != null) {
+                    mAnswerRankBll.hideRankList();
+                }
             }
         };
         if (type == XESCODE.STOPQUESTION) {
             mAnswerRankBll.getFullMarkListQuestion(callBack);
         } else if (type == XESCODE.EXAM_STOP) {
             mAnswerRankBll.getFullMarkListTest(callBack);
+        if (mAnswerRankBll != null) {
+            if (type == XESCODE.STOPQUESTION) {
+                mAnswerRankBll.getFullMarkListQuestion(callBack);
+            } else if (type == XESCODE.EXAM_STOP) {
+                mAnswerRankBll.getFullMarkListTest(callBack);
+            }
         }
     }
 
