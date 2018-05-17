9040f3e50883a018469622c5c9d302d7a4b3b02c
yuanzhanli <yuanzhanli@100tal.com>
2018-05-17 20:59:20 +0800

    socket地址去掉端口号

--- src/main/java/com/xueersi/parentsmeeting/modules/livevideo/business/RolePlayerBll.java
+++ src/main/java/com/xueersi/parentsmeeting/modules/livevideo/business/RolePlayerBll.java
@@ -71,7 +71,7 @@ public class RolePlayerBll extends BaseBll implements RolePlayAction {
      * 连接地址
      */
     private String webSocketUrl = "ws://wsarts.xueersi" +
-            ".com:9590/roleplay/index?userId=%1$s&role=1&cookie=%2$s&liveId=%3$s";
+            ".com/roleplay/index?userId=%1$s&role=1&cookie=%2$s&liveId=%3$s";
 
     /**
      * RolePlayer数据实体
