153877cf0ba30b0d904801d83557e6cc13ec15a3
linyuqiang <917866068@qq.com>
2018-07-10 21:28:39 +0800

    Mod:互动题预加载
    (cherry picked from commit ba46de8)

--- src/main/java/com/xueersi/parentsmeeting/modules/livevideo/question/business/QuestionWebCache.java
+++ src/main/java/com/xueersi/parentsmeeting/modules/livevideo/question/business/QuestionWebCache.java
@@ -32,7 +32,7 @@ public class QuestionWebCache {
     public void startCache() {
         CacheWebView webView = new CacheWebView(context);
         webView.setWebViewClient(new MyWebViewClient());
-//        webView.loadUrl("https://live.xueersi.com/science/Live/getMultiTestPaper?liveId=119740&testId=365160-1&stuId=-111&stuName=test@talwx.com&stuCouId=12345654&isArts=0&nonce=45645dasf&isTowall=0");
+        webView.loadUrl("https://live.xueersi.com/science/Live/getMultiTestPaper?liveId=119740&testId=365160-1&stuId=-111&stuName=test@talwx.com&stuCouId=12345654&isArts=0&nonce=45645dasf&isTowall=0");
     }
 
     private class MyWebViewClient extends ErrorWebViewClient {
@@ -43,6 +43,7 @@ public class QuestionWebCache {
 
         @Override
         public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
+            Loger.d(TAG, "shouldInterceptRequest:url=" + url);
             return super.shouldInterceptRequest(view, url);
         }
 
