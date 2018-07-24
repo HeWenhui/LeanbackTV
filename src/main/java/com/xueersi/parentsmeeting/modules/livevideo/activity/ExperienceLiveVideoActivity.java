7d114c0800aeacd0dfd790f3afeb30421027bcfa
Dengwei <dengwei1@100tal.com>
2018-07-24 17:13:43 +0800

    体验课添加播放失败的日志

--- src/main/java/com/xueersi/parentsmeeting/modules/livevideo/activity/ExperienceLiveVideoActivity.java
+++ src/main/java/com/xueersi/parentsmeeting/modules/livevideo/activity/ExperienceLiveVideoActivity.java
@@ -2046,5 +2046,7 @@ public class ExperienceLiveVideoActivity extends LiveVideoActivityBase implement
         super.resultFailed(arg1, arg2);
         resultFailed = true;
         mIsShowQuestion = mIsShowRedpacket = false;
+        Loger.d(this,TAG, "liveId=" + mVideoEntity.getLiveId() + "," +
+                "url=" + Uri.parse(mWebPath),true);
     }
 }
