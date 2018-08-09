ce72adb0b91da871d6770400b99842e9d45fbd7c
zhangyueyi <zhangyueyi@100tal.com>
2018-08-09 10:21:08 +0800

    回放小英聊天区添加MMD皮肤
    Change-Id: If8a8763bba63796b58984b670055305209394f19

---
+++ src/main/java/com/xueersi/parentsmeeting/modules/livevideo/message/business/LiveMessageBackBll.java
@@ -0,0 +1,33 @@
+package com.xueersi.parentsmeeting.modules.livevideo.message.business;
+
+import android.app.Activity;
+import android.widget.RelativeLayout;
+
+import com.xueersi.parentsmeeting.modules.livevideo.business.LiveBackBaseBll;
+import com.xueersi.parentsmeeting.modules.livevideo.business.LiveBackBll;
+import com.xueersi.parentsmeeting.modules.livevideo.message.pager.LiveBackVideoMessagePager;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+//直播回放，在聊天区加上MMD皮肤
+public class LiveMessageBackBll extends LiveBackBaseBll {
+    //是否是小英，使用MMD皮肤
+    private boolean isSmallEnglish = false;
+
+    public LiveMessageBackBll(Activity activity, LiveBackBll liveBackBll) {
+        super(activity, liveBackBll);
+    }
+
+    @Override
+    public void initView(RelativeLayout bottomContent, AtomicBoolean mIsLand) {
+        super.initView(bottomContent, mIsLand);
+        if (liveGetInfo != null) {
+            isSmallEnglish = liveGetInfo.getSmallEnglish();
+        }
+        if (isSmallEnglish) {
+            LiveBackVideoMessagePager liveBackVideoMessagePager = new LiveBackVideoMessagePager((mContext));
+            bottomContent.addView(liveBackVideoMessagePager.getRootView(), liveBackVideoMessagePager
+                    .getBoardParams());
+        }
+    }
+}
