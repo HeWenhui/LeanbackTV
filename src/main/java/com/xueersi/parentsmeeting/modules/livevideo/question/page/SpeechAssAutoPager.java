2a8503c531d12b4407dcd4668de0b5c934f879cd
Dengwei <dengwei1@100tal.com>
2018-09-12 10:32:24 +0800

    传递试题ID和评测分数
    Change-Id: I5e5b3e7492fe9415e0909c9a8324310dc013f9ff

--- src/main/java/com/xueersi/parentsmeeting/modules/livevideo/question/page/SpeechAssAutoPager.java
+++ src/main/java/com/xueersi/parentsmeeting/modules/livevideo/question/page/SpeechAssAutoPager.java
@@ -33,6 +33,7 @@ import com.xueersi.common.entity.BaseVideoQuestionEntity;
 import com.xueersi.common.http.ResponseEntity;
 import com.xueersi.parentsmeeting.modules.livevideo.R;
 import com.xueersi.parentsmeeting.modules.livevideo.core.LivePagerBack;
+import com.xueersi.parentsmeeting.modules.livevideo.event.VoiceAnswerResultEvent;
 import com.xueersi.parentsmeeting.modules.livevideo.question.business.OnSpeechEval;
 import com.xueersi.parentsmeeting.modules.livevideo.question.business.SpeechEvalAction;
 import com.xueersi.parentsmeeting.modules.livevideo.config.LiveVideoConfig;
@@ -43,6 +44,7 @@ import com.xueersi.common.speech.SpeechEvaluatorUtils;
 import com.xueersi.lib.framework.utils.file.FileUtils;
 import com.xueersi.parentsmeeting.modules.livevideo.util.Loger;
 
+import org.greenrobot.eventbus.EventBus;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -634,6 +636,8 @@ public class SpeechAssAutoPager extends BaseSpeechAssessmentPager {
                 detail.put("total_score", score);
                 answers1.put("detail", detail);
                 answers.put("1", answers1);
+                // 发送分数和TestId
+                EventBus.getDefault().post(new VoiceAnswerResultEvent(id,score));
                 speechEvalAction.sendSpeechEvalResult2(id, answers.toString(), new OnSpeechEval() {
                     OnSpeechEval onSpeechEval = this;
 
