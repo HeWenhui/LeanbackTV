1d05d398a2b6cd49ad7056d85183bf4ccab25dbe
zhangyueyi <zhangyueyi@100tal.com>
2018-11-07 22:08:08 +0800

    新增体验课懂了么弹窗

--- src/main/java/com/xueersi/parentsmeeting/modules/livevideo/understand/page/SmallChineseUnderstandPager.java
+++ src/main/java/com/xueersi/parentsmeeting/modules/livevideo/understand/page/SmallChineseUnderstandPager.java
@@ -3,12 +3,15 @@ package com.xueersi.parentsmeeting.modules.livevideo.understand.page;
 import android.view.View;
 
 import com.xueersi.common.base.BasePager;
+import com.xueersi.parentsmeeting.modules.livevideo.R;
 
 public class SmallChineseUnderstandPager extends BasePager {
     @Override
     public View initView() {
+        View view = View.inflate(mContext, R.layout.page_livevideo_small_chinese_understand, null);
 
-        return null;
+
+        return view;
     }
 
     @Override
