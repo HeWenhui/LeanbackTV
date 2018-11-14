9102b6982544d009a3df9c63b8dab4e684e5a3d6
linyuqiang <qq100250>
2018-11-14 19:18:07 +0800

    Mod:英语战队pk胜利界面

--- src/main/java-en_teampk/com/xueersi/parentsmeeting/modules/livevideo/enteampk/pager/TeamPkLeadPager.java
+++ src/main/java-en_teampk/com/xueersi/parentsmeeting/modules/livevideo/enteampk/pager/TeamPkLeadPager.java
@@ -3,6 +3,7 @@ package com.xueersi.parentsmeeting.modules.livevideo.enteampk.pager;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.xueersi.parentsmeeting.modules.livevideo.R;
 import com.xueersi.parentsmeeting.modules.livevideo.page.LiveBasePager;
@@ -17,13 +18,28 @@ public class TeamPkLeadPager extends LiveBasePager {
 
     @Override
     public View initView() {
-        View view= LayoutInflater.from(mContext).inflate(R.layout.page_livevideo_en_team_lead,null);
+        View view = LayoutInflater.from(mContext).inflate(R.layout.page_livevideo_en_team_lead, null);
         return view;
     }
 
     @Override
     public void initData() {
         super.initData();
+        mView.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ViewGroup group = (ViewGroup) mView;
+                final View view = LayoutInflater.from(mContext).inflate(R.layout.layout_livevideo_en_team_lead_win, group, false);
+                group.addView(view);
+                mView.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        ViewGroup group = (ViewGroup) mView;
+                        group.removeView(view);
+                    }
+                }, 2500);
+            }
+        }, 2000);
     }
 
     @Override
