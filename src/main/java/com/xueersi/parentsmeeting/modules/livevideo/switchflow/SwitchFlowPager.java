334105d6ac06f3c63ce8a7a70b6a231afda65e0c
zhangyueyi <zhangyueyi@100tal.com>
2018-11-27 18:42:13 +0800

    Merge remote-tracking branch 'origin/feature_chinese_skin' into feature_switch_flow
    # Conflicts:
    #	src/main/java/com/xueersi/parentsmeeting/modules/livevideo/business/LiveVideoAction.java

--- src/main/java/com/xueersi/parentsmeeting/modules/livevideo/switchflow/SwitchFlowPager.java
+++ src/main/java/com/xueersi/parentsmeeting/modules/livevideo/switchflow/SwitchFlowPager.java
@@ -2,11 +2,18 @@ package com.xueersi.parentsmeeting.modules.livevideo.switchflow;
 
 import android.content.Context;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.xueersi.common.base.BasePager;
 import com.xueersi.parentsmeeting.modules.livevideo.R;
 
 public class SwitchFlowPager extends BasePager {
+    /** 出现时从右往左侧滑的动画 */
+    private Animation animationIn;
+
+    /** 消失时从左往右侧滑的动画 */
+    private Animation animationOut;
 
     public SwitchFlowPager(Context context) {
         super(context);
@@ -25,6 +32,7 @@ public class SwitchFlowPager extends BasePager {
 
     @Override
     public void initData() {
-
+        animationIn = AnimationUtils.loadAnimation(mContext, R.anim.anim_livevideo_rank_in);
+        animationOut = AnimationUtils.loadAnimation(mContext, R.anim.anim_livevideo_rank_out);
     }
 }
