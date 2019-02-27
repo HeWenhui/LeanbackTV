c1d27b4ae254ac07adb9577b3eb72bca48df8fb8
chenkun2 <chenkun2@100tal.com>
2019-02-27 11:30:49 +0800

    mod:新增字段 直播半身直播体验课 H5 url地址动态从接口中获取

--- src/main/java/com/xueersi/parentsmeeting/modules/livevideo/question/business/EnglishH5HalfBodyExperienceBll.java
+++ src/main/java/com/xueersi/parentsmeeting/modules/livevideo/question/business/EnglishH5HalfBodyExperienceBll.java
@@ -40,10 +40,15 @@ public class EnglishH5HalfBodyExperienceBll extends LiveBackBaseBll {
      **/
     private String mTermid;
     private VideoLivePlayBackEntity mRoomInitData;
+    /**
+     * 半身直播体验课 H5 域名
+     */
+    private String mHalfBodyUrl;
 
-    public EnglishH5HalfBodyExperienceBll(Activity activity, LiveBackBll liveBackBll, String termId) {
+    public EnglishH5HalfBodyExperienceBll(Activity activity, LiveBackBll liveBackBll, String termId,String halfbodyUrl) {
         super(activity, liveBackBll);
         mTermid = termId;
+        mHalfBodyUrl = halfbodyUrl;
     }
 
     @Override
@@ -68,6 +73,7 @@ public class EnglishH5HalfBodyExperienceBll extends LiveBackBaseBll {
         liveBaseEnglishH5CoursewareCreat.setArts(isArts);
         liveBaseEnglishH5CoursewareCreat.setWrapOnH5ResultClose(new WrapOnH5ResultClose(activity));
         liveBaseEnglishH5CoursewareCreat.setLivePagerBack(englishH5CoursewareBll);
+        liveBaseEnglishH5CoursewareCreat.setLiveGetInfo(liveGetInfo);
         englishH5CoursewareBll.setBaseEnglishH5CoursewareCreat(liveBaseEnglishH5CoursewareCreat);
     }
 
@@ -177,7 +183,8 @@ public class EnglishH5HalfBodyExperienceBll extends LiveBackBaseBll {
                 e.printStackTrace();
             }
         }
-        stringBuilder.append(LiveVideoConfig.URL_HALFBODY_EXPERIENCE_LIVE_H5)
+        String url = TextUtils.isEmpty(mHalfBodyUrl)?LiveVideoConfig.URL_HALFBODY_EXPERIENCE_LIVE_H5:mHalfBodyUrl;
+        stringBuilder.append(url)
                 .append("?stuId=").append(stuId)
                 .append("&liveId=").append(mVideoEntity.getLiveId())
                 .append("&packageSource=").append(packageSource)
