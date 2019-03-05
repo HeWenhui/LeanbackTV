96ec52d20b07678a7b020cfdfb7067ab7a5fe84f
zhangyueyi <zhangyueyi@100tal.com>
2019-03-05 15:15:10 +0800

    add:修改方法对应模块优化

--- src/main/java/com/xueersi/parentsmeeting/modules/livevideo/message/pager/ExperLiveMessageStandPager.java
+++ src/main/java/com/xueersi/parentsmeeting/modules/livevideo/message/pager/ExperLiveMessageStandPager.java
@@ -43,6 +43,8 @@ import com.tal.speech.speechrecognizer.EvaluatorListener;
 import com.tal.speech.speechrecognizer.ResultCode;
 import com.tal.speech.speechrecognizer.ResultEntity;
 import com.tal.speech.speechrecognizer.SpeechParamEntity;
+import com.tal.speech.utils.SpeechEvaluatorUtils;
+import com.tal.speech.utils.SpeechUtils;
 import com.xueersi.common.base.AbstractBusinessDataCallBack;
 import com.xueersi.common.http.HttpCallBack;
 import com.xueersi.common.http.ResponseEntity;
@@ -50,9 +52,6 @@ import com.xueersi.common.permission.PermissionCallback;
 import com.xueersi.common.permission.XesPermission;
 import com.xueersi.common.permission.config.PermissionConfig;
 import com.xueersi.common.sharedata.ShareDataManager;
-import com.xueersi.common.speech.SpeechConfig;
-import com.xueersi.common.speech.SpeechEvaluatorUtils;
-import com.xueersi.common.speech.SpeechUtils;
 import com.xueersi.component.cloud.XesCloudUploadBusiness;
 import com.xueersi.component.cloud.config.CloudDir;
 import com.xueersi.component.cloud.config.XesCloudConfig;
@@ -108,6 +107,10 @@ import java.util.Map;
 import cn.dreamtobe.kpswitch.util.KPSwitchConflictUtil;
 import cn.dreamtobe.kpswitch.util.KeyboardUtil;
 import cn.dreamtobe.kpswitch.widget.KPSwitchFSPanelLinearLayout;
+
+//import com.xueersi.common.speech.SpeechConfig;
+//import com.xueersi.common.speech.SpeechEvaluatorUtils;
+//import com.xueersi.common.speech.SpeechUtils;
 
 /**
  * @author linyuqiang
@@ -742,7 +745,12 @@ public class ExperLiveMessageStandPager extends BaseLiveMessagePager implements 
             mSpeechUtils.setLanguage(Constants.ASSESS_PARAM_LANGUAGE_EN);
         }
         mParam = new SpeechParamEntity();
-        mSpeechUtils.setOnFileSuccess(new SpeechEvaluatorUtils.OnFileSuccess() {
+        mSpeechUtils.prepar(new SpeechEvaluatorUtils.OnFileSuccess() {
+            @Override
+            public void onFileInit(int code) {
+
+            }
+
             @Override
             public void onFileSuccess() {
                 mSpeechFail = "模型正在启动，请稍后";
@@ -765,18 +773,20 @@ public class ExperLiveMessageStandPager extends BaseLiveMessagePager implements 
                                         isShowSpeechRecog = (mRecogtestEndTime - mRecogtestBeginTime) < 3000l ? true
                                                 : false;
                                         if (isShowSpeechRecog) {
-                                            mSdm.put(SpeechEvaluatorUtils.RECOG_RESULT, isShowSpeechRecog, ShareDataManager.SHAREDATA_USER);
+                                            mSdm.put(SpeechEvaluatorUtils.RECOG_RESULT, isShowSpeechRecog,
+                                                    ShareDataManager.SHAREDATA_USER);
                                             initOpenBt(false, true);
                                         }
                                     }
                                 }
+
                                 @Override
                                 public void onVolumeUpdate(int volume) {
 
                                 }
                             });
                         }
-                    },4000);
+                    }, 4000);
                 }
             }
 
@@ -785,7 +795,6 @@ public class ExperLiveMessageStandPager extends BaseLiveMessagePager implements 
                 mSpeechFail = "模型启动失败，请使用手动输入";
             }
         });
-
 //        SpeechEvaluatorUtils.setOnFileSuccess(new SpeechEvaluatorUtils.OnFileSuccess() {
 //            @Override
 //            public void onFileSuccess() {
