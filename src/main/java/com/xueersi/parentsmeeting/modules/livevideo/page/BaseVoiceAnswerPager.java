8724ce1b7fbf61cdab7f48c9811c795239857997
linyuqiang <qq100250>
2019-03-06 10:09:27 +0800

    Mod:静音
    Change-Id: I4f360b96100c43f9d85dd325c00ec2520ebe2267

--- src/main/java/com/xueersi/parentsmeeting/modules/livevideo/page/BaseVoiceAnswerPager.java
+++ src/main/java/com/xueersi/parentsmeeting/modules/livevideo/page/BaseVoiceAnswerPager.java
@@ -15,13 +15,13 @@ public abstract class BaseVoiceAnswerPager extends LiveBasePager {
 
     public BaseVoiceAnswerPager(Context context) {
         super(context);
-//        BasePlayerFragment videoFragment = ProxUtil.getProxUtil().get(context, BasePlayerFragment.class);
-//        if (videoFragment != null) {
-//            videoFragment.setVolume(0, 0);
-//            logger.d(TAG + ":setVolume:0");
-//        } else {
-//            logger.d(TAG + ":setVolume:null");
-//        }
+        BasePlayerFragment videoFragment = ProxUtil.getProxUtil().get(context, BasePlayerFragment.class);
+        if (videoFragment != null) {
+            videoFragment.setVolume(0, 0);
+            logger.d(TAG + ":setVolume:0");
+        } else {
+            logger.d(TAG + ":setVolume:null");
+        }
     }
 
     public abstract void setIse(SpeechUtils mIse);
@@ -46,12 +46,12 @@ public abstract class BaseVoiceAnswerPager extends LiveBasePager {
     public void onDestroy() {
         super.onDestroy();
         BasePlayerFragment videoFragment = ProxUtil.getProxUtil().get(mContext, BasePlayerFragment.class);
-//        if (videoFragment != null) {
-//            videoFragment.setVolume(VP.DEFAULT_STEREO_VOLUME, VP.DEFAULT_STEREO_VOLUME);
-//            logger.d("onDestroy:setVolume:1");
-//        } else {
-//            logger.d("onDestroy:setVolume:null");
-//        }
+        if (videoFragment != null) {
+            videoFragment.setVolume(VP.DEFAULT_STEREO_VOLUME, VP.DEFAULT_STEREO_VOLUME);
+            logger.d("onDestroy:setVolume:1");
+        } else {
+            logger.d("onDestroy:setVolume:null");
+        }
     }
 
 }
