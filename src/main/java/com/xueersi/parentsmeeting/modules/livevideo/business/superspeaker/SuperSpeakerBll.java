56f97eda005bb714c1b948c86782b50f528da4ab
zhangyueyi <zhangyueyi@100tal.com>
2019-04-16 16:48:49 +0800

    add:添加超级演讲家bll

---
+++ src/main/java/com/xueersi/parentsmeeting/modules/livevideo/business/superspeaker/SuperSpeakerBll.java
@@ -0,0 +1,18 @@
+package com.xueersi.parentsmeeting.modules.livevideo.business.superspeaker;
+
+import android.app.Activity;
+
+import com.xueersi.parentsmeeting.modules.livevideo.business.LiveBaseBll;
+import com.xueersi.parentsmeeting.modules.livevideo.core.LiveBll2;
+
+public class SuperSpeakerBll extends LiveBaseBll {
+    public SuperSpeakerBll(Activity context, LiveBll2 liveBll) {
+        super(context, liveBll);
+    }
+
+    public void preCamera() {
+
+    }
+
+
+}
