49c3e99ca89fc7ef29f5c8765640eeaf4681a35b
linyuqiang <qq100250>
2019-04-30 21:14:23 +0800

    Mod:语音炮弹日志
    Change-Id: I8c4811241bc196dc53078e7ec2d2077a392f7ecf

--- src/main/java/com/xueersi/parentsmeeting/modules/livevideoOldIJK/groupgame/pager/GroupGameMultNativePager.java
+++ src/main/java/com/xueersi/parentsmeeting/modules/livevideoOldIJK/groupgame/pager/GroupGameMultNativePager.java
@@ -37,8 +37,8 @@ import com.xueersi.lib.framework.utils.file.FileUtils;
 import com.xueersi.parentsmeeting.module.videoplayer.media.VP;
 import com.xueersi.parentsmeeting.modules.livevideo.R;
 import com.xueersi.parentsmeeting.modules.livevideoOldIJK.business.AudioRequest;
-import com.xueersi.parentsmeeting.modules.livevideo.business.ContextLiveAndBackDebug;
-import com.xueersi.parentsmeeting.modules.livevideo.business.LiveAndBackDebug;
+import com.xueersi.parentsmeeting.modules.livevideoOldIJK.business.ContextLiveAndBackDebug;
+import com.xueersi.parentsmeeting.modules.livevideoOldIJK.business.LiveAndBackDebug;
 import com.xueersi.parentsmeeting.modules.livevideo.business.XESCODE;
 import com.xueersi.parentsmeeting.modules.livevideo.business.agora.AGEventHandler;
 import com.xueersi.parentsmeeting.modules.livevideo.business.agora.WorkerThreadPool;
@@ -86,6 +86,7 @@ import com.xueersi.parentsmeeting.modules.livevideo.util.LiveAudioManager;
 import com.xueersi.parentsmeeting.modules.livevideo.util.LiveCacheFile;
 import com.xueersi.parentsmeeting.modules.livevideo.util.ProxUtil;
 import com.xueersi.parentsmeeting.modules.livevideoOldIJK.fragment.BasePlayerFragment;
+import com.xueersi.parentsmeeting.modules.livevideoOldIJK.stablelog.GroupGameLog;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -169,7 +170,7 @@ public class GroupGameMultNativePager extends BaseCoursewareNativePager implemen
      * 在网页中嵌入js，只嵌入一次
      */
     private boolean addJs = false;
-
+    private boolean isForce = false;
     private int mPagerIndex = 0;
     private InteractiveTeam interactiveTeam;
     private WorkerThreadPool mWorkerThread;
@@ -253,6 +254,7 @@ public class GroupGameMultNativePager extends BaseCoursewareNativePager implemen
         if (LiveQueConfig.EN_COURSE_TYPE_CLEANING_UP.equals(gameType)) {
             multModeAction = new CleanUpOnMessage();
         } else {
+            GroupGameLog.sno2(liveAndBackDebug, detailInfo.id, 1);
             multModeAction = new VoiceCannonOnMessage();
         }
 //        startSpeechRecognize();
@@ -378,6 +380,7 @@ public class GroupGameMultNativePager extends BaseCoursewareNativePager implemen
 //                jsonData.put("studentNum", -1);
                         jsonData.put("turnToPageNum", currentAnswerIndex);
                         wvSubjectWeb.loadUrl("javascript:postMessage(" + jsonData + ",'" + "*" + "')");
+                        GroupGameLog.sno4(liveAndBackDebug, detailInfo.id, currentAnswerIndex + "", 1);
                     }
                     mLogtf.d("VoiceCannonTurnRun:pagerNum=" + pagerNum + ",currentAnswerIndex=" + currentAnswerIndex + ",remove=" + remove);
                 }
@@ -416,6 +419,8 @@ public class GroupGameMultNativePager extends BaseCoursewareNativePager implemen
             if (enterTime == 0) {
                 enterTime = System.currentTimeMillis();
             }
+            GroupGameLog.sno3(liveAndBackDebug, detailInfo.id, 1);
+            GroupGameLog.sno4(liveAndBackDebug, detailInfo.id, "0", 1);
             final GroupGameTestInfosEntity.TestInfoEntity test = tests.get(0);
             try {
                 mLogtf.d("onLoadComplete:totaltime=" + test.getTotalTime());
@@ -1234,6 +1239,7 @@ public class GroupGameMultNativePager extends BaseCoursewareNativePager implemen
 
     @Override
     public void submitData() {
+        isForce = true;
         submit();
     }
 
@@ -1364,6 +1370,7 @@ public class GroupGameMultNativePager extends BaseCoursewareNativePager implemen
                 //小于0直接结束
                 mLogtf.d("getCourseWareTests:total=" + testEntity.getTotalTime() + ",nowPlayTime=" + nowPlayTime);
                 if (lastTime < 0) {
+                    isForce = true;
                     showResult = false;
                     testEntity.setTotalTime(0);
                     allAnswerList.clear();
@@ -1417,7 +1424,6 @@ public class GroupGameMultNativePager extends BaseCoursewareNativePager implemen
                     ispreload = true;
                 }
                 connectTcp();
-                NewCourseLog.sno3(liveAndBackDebug, NewCourseLog.getNewCourseTestIdSec(detailInfo, isArts), getSubtestid(), testEntity.getPreviewPath(), ispreload, testEntity.getTestId());
             }
 
             private void connectTcp() {
@@ -1702,6 +1708,10 @@ public class GroupGameMultNativePager extends BaseCoursewareNativePager implemen
             starNum = 5;
         }
         mLogtf.d("submit:averageScore=" + averageScore + ",starNum=" + starNum + ",energy=" + energy);
+        if (LiveQueConfig.EN_COURSE_TYPE_VOICE_CANNON.equals(gameType)) {
+            GroupGameLog.sno5(liveAndBackDebug, detailInfo.id, isForce ? "endPublish" : "autoSubmit", voiceTime == 0 ?
+                    "0" : "1", 1);
+        }
         englishH5CoursewareSecHttp.submitGroupGame(detailInfo, 1, voiceTime, teamEntity.getPkTeamId(), gameGroupId, starNum, energy, gold,
                 videoLengthTime, micLengthTime
                 , acceptVideoLengthTime, acceptMicLengthTime, answerData.toString(), new AbstractBusinessDataCallBack() {
@@ -1719,6 +1729,9 @@ public class GroupGameMultNativePager extends BaseCoursewareNativePager implemen
                                     onClose.onH5ResultClose(GroupGameMultNativePager.this, detailInfo);
                                 }
                             });
+                            if (LiveQueConfig.EN_COURSE_TYPE_VOICE_CANNON.equals(gameType)) {
+                                GroupGameLog.sno6(liveAndBackDebug, detailInfo.id, "" + entities.size(), 1);
+                            }
                         } else {
                             onClose.onH5ResultClose(GroupGameMultNativePager.this, detailInfo);
                         }
@@ -2313,6 +2326,7 @@ public class GroupGameMultNativePager extends BaseCoursewareNativePager implemen
                                                     jsonData.put("score", score);
                                                     jsonData.put("studentNum", finalStudentNum);
                                                     if (isTurnPage) {
+                                                        GroupGameLog.sno4(liveAndBackDebug, detailInfo.id, currentAnswerIndex + "", 1);
                                                         jsonData.put("turnToPageNum", currentAnswerIndex);
                                                     } else {
                                                         jsonData.put("turnToPageNum", -1);
--- src/main/java/com/xueersi/parentsmeeting/modules/livevideoOldIJK/stablelog/GroupGameLog.java
+++ src/main/java/com/xueersi/parentsmeeting/modules/livevideoOldIJK/stablelog/GroupGameLog.java
@@ -11,9 +11,10 @@ public class GroupGameLog {
     /**
      * 学生收到题目信息（SNO 2）   ---交互日志
      */
-    public static void sno2(LiveAndBackDebug liveAndBackDebug, String testid) {
+    public static void sno2(LiveAndBackDebug liveAndBackDebug, String testid, int signal) {
         StableLogHashMap logHashMap = new StableLogHashMap("voicecannonReceive");
         logHashMap.put("testid", testid);
+        logHashMap.put("signal", "" + signal);
         logHashMap.put("sno", "2");
         liveAndBackDebug.umsAgentDebugInter(eventId, logHashMap.getData());
     }
@@ -21,9 +22,10 @@ public class GroupGameLog {
     /**
      * 开始作答时间---展示日志
      */
-    public static void sno3(LiveAndBackDebug liveAndBackDebug, String testid) {
+    public static void sno3(LiveAndBackDebug liveAndBackDebug, String testid, int signal) {
         StableLogHashMap logHashMap = new StableLogHashMap("voicecannonAnswer");
         logHashMap.put("testid", testid);
+        logHashMap.put("signal", "" + signal);
         logHashMap.put("sno", "3");
         liveAndBackDebug.umsAgentDebugInter(eventId, logHashMap.getData());
     }
@@ -31,10 +33,11 @@ public class GroupGameLog {
     /**
      * 翻页时间---交互
      */
-    public static void sno4(LiveAndBackDebug liveAndBackDebug, String testid, String subtestid) {
+    public static void sno4(LiveAndBackDebug liveAndBackDebug, String testid, String subtestid, int signal) {
         StableLogHashMap logHashMap = new StableLogHashMap("voicecannonNextPage");
         logHashMap.put("testid", testid);
         logHashMap.put("subtestid", subtestid);
+        logHashMap.put("signal", "" + signal);
         logHashMap.put("sno", "4");
         liveAndBackDebug.umsAgentDebugInter(eventId, logHashMap.getData());
     }
@@ -43,20 +46,22 @@ public class GroupGameLog {
      * 游戏结束时间（SNO 5）---交互日志
      */
     public static void sno5(LiveAndBackDebug liveAndBackDebug, String testid, String state,
-                            String hasvoicetime) {
+                            String hasvoicetime, int signal) {
         StableLogHashMap logHashMap = new StableLogHashMap("voicecannonSubmit");
         logHashMap.put("testid", testid);
         logHashMap.put("state", state);
         logHashMap.put("hasvoicetime", hasvoicetime);
+        logHashMap.put("signal", "" + signal);
         logHashMap.put("sno", "5");
         liveAndBackDebug.umsAgentDebugInter(eventId, logHashMap.getData());
     }
 
     /** sno6显示结果页 */
-    public static void sno6(LiveAndBackDebug liveAndBackDebug, String testid, String mvpnum) {
+    public static void sno6(LiveAndBackDebug liveAndBackDebug, String testid, String mvpnum, int signal) {
         StableLogHashMap logHashMap = new StableLogHashMap("voicecannonShowMvp");
         logHashMap.put("testid", testid);
         logHashMap.put("mvpnum", "" + mvpnum);
+        logHashMap.put("signal", "" + signal);
         logHashMap.put("sno", "6");
         liveAndBackDebug.umsAgentDebugInter(eventId, logHashMap.getData());
     }
