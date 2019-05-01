1d46c7f05331124f1efb2640096e08fe11c5bc29
huadongliang <huadongliang@100tal.com>
2019-05-01 20:59:38 +0800

    mod:出门测开发。
    Change-Id: I1fb8b1a96db3cf1b9afa52ea7bf11ae333ee755b

--- src/main/java/com/xueersi/parentsmeeting/modules/livevideoOldIJK/question/page/EnglishH5CoursewareX5Pager.java
+++ src/main/java/com/xueersi/parentsmeeting/modules/livevideoOldIJK/question/page/EnglishH5CoursewareX5Pager.java
@@ -460,8 +460,12 @@ public class EnglishH5CoursewareX5Pager extends BaseWebviewX5Pager implements Ba
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
+                        String tutorId = liveId;
+                        if (!TextUtils.isEmpty(tutorId)) {
+                            tutorId=  tutorId.replace("_t","");
+                        }
                         String realurl = englishH5Entity.getDynamicurl();
-                        mLoadUrls = realurl + "?stuId=" + stuId + "&liveId=" + liveId + "&stuCouId=" + stuCouId +
+                        mLoadUrls = realurl + "?stuId=" + stuId + "&liveId=" + tutorId + "&stuCouId=" + stuCouId +
                                 "&classId=" + classId + "&teamId=" + teamId + "&packageId=" + packageId +
                                 "&packageSource=" + packageSource + "&packageAttr=" + packageAttr +
                                 "&releasedPageInfos=" + releasedPageInfos + "&classTestId=" + classTestId +
@@ -515,6 +519,8 @@ public class EnglishH5CoursewareX5Pager extends BaseWebviewX5Pager implements Ba
                     } else {
                         dynamicurl = TextUtils.isEmpty(LiveVideoConfig.LIVEMULH5URL) ? defaulturl : LiveVideoConfig.LIVEMULH5URL;
                     }
+
+
                     mLoadUrls = dynamicurl + "?stuId=" + stuId + "&liveId=" + liveId + "&stuCouId=" + stuCouId +
                             "&classId=" + classId + "&teamId=" + teamId + "&packageId=" + packageId +
                             "&packageSource=" + packageSource + "&packageAttr=" + packageAttr + "&releasedPageInfos="
