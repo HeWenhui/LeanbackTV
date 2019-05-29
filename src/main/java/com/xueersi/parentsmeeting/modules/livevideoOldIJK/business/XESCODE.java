44325eebcd569020ea1f2010bbc8ae2f18361f2c
linyuqiang <qq100250>
2019-05-29 23:10:20 +0800

    Mod:xescode
    Change-Id: Iff518021936bc210730112d5091366df004760be

--- src/main/java/com/xueersi/parentsmeeting/modules/livevideoOldIJK/business/XESCODE.java
+++ src/main/java/com/xueersi/parentsmeeting/modules/livevideoOldIJK/business/XESCODE.java
@@ -2,8 +2,4 @@ package com.xueersi.parentsmeeting.modules.livevideoOldIJK.business;
 
 @Deprecated
 public interface XESCODE extends com.xueersi.parentsmeeting.modules.livevideo.business.XESCODE{
-    /** 开启 */
-    String ON = "on";
-    /** 关闭 */
-    String OFF = "off";
 }
