1746f0a1d4c0d8bf5657172d33def9473f936d13
linyuqiang <qq100250>
2019-06-05 14:20:43 +0800

    Mod:irc连接日志。
    Change-Id: I5ad129d8640e340938e0af6ffcc485d25a17ded5

---
+++ src/main/java/com/xueersi/parentsmeeting/modules/livevideo/config/SysLogLable.java
@@ -0,0 +1,18 @@
+package com.xueersi.parentsmeeting.modules.livevideo.config;
+
+/**
+ * 系统日志埋点
+ * http://wiki.xesv5.com/pages/viewpage.action?pageId=18910075
+ */
+public class SysLogLable {
+    /** IRC连接 */
+    public static String connectIrcServer = "connectIrcServer";
+    /** IRC连接成功 */
+    public static String connectIRCSuccess = "connectIRCSuccess";
+    /** IRC连接失败 */
+    public static String connectIRCDidFailed = "connectIRCDidFailed";
+    /** 接收到topic消息 */
+    public static String receivedMessageOfTopic = "receivedMessageOfTopic";
+    /** 接收到notice消息 */
+    public static String receivedMessageOfNotic = "receivedMessageOfNotic";
+}
