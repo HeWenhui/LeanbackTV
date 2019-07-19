3d121a1d233cee6a4fd004b2953b1872d2bcc5c2
linyuqiang <qq100250>
2019-07-19 16:52:05 +0800

    Mod:修改类名
    Change-Id: Id7d833d608f2645a6ab3144a7f1a321893c1e4e8

--- src/main/english/java-en_tcp/com/xueersi/parentsmeeting/modules/livevideo/lib/LiveTcpLoggerFactory.java
+++ src/main/english/java-en_tcp/com/xueersi/parentsmeeting/modules/livevideo/lib/LiveTcpLoggerFactory.java
@@ -3,7 +3,7 @@ package com.xueersi.parentsmeeting.modules.livevideo.lib;
 /**
  *tcp 暂时的日志，为了在异常的时候被bugly上传
  */
-public class LiveLoggerFactory {
+public class LiveTcpLoggerFactory {
     public static Logger getLogger(String tag) {
         return new Logger(tag);
     }
