53188ba247f4824a4bbcccff08d8694422953d85
yuanwei2 <yw123456>
2019-08-08 18:35:53 +0800

    mod:还原代码

--- src/main/java/com/xueersi/parentsmeeting/modules/livevideo/chpk/page/PkTeamSelectPager.java
+++ src/main/java/com/xueersi/parentsmeeting/modules/livevideo/chpk/page/PkTeamSelectPager.java
@@ -776,7 +776,6 @@ public class PkTeamSelectPager extends BasePager implements View.OnClickListener
     }
 
     private void releaseRes() {
-        stopMusic(R.raw.war_bg);
 
         try {
             releaseSoundRes();
