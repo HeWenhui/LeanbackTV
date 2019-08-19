c8d59c93a0ff6e22709df0bba06f34e965637d2a
linyuqiang <qq100250>
2019-08-19 20:42:16 +0800

    Mod:直播接口公共参数。

--- src/main/java-core/com/xueersi/parentsmeeting/modules/livevideo/http/LiveHttpAction.java
+++ src/main/java-core/com/xueersi/parentsmeeting/modules/livevideo/http/LiveHttpAction.java
@@ -8,4 +8,8 @@ public interface LiveHttpAction {
     LiveVideoSAConfig.Inner getLiveVideoSAConfigInner();
 
     void sendPostDefault(final String url, final HttpRequestParams httpRequestParams, HttpCallBack httpCallBack);
+
+    void sendJsonPost(final String url, final Object paramObject, HttpCallBack httpCallBack);
+
+    void sendJsonPostDefault(final String url, final HttpRequestParams httpRequestParams, HttpCallBack httpCallBack);
 }
