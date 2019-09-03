c010869131ee0919951307bc17555d7251ebe75f
chenkun2 <chenkun2@100tal.com>
2019-09-03 12:23:22 +0800

    mod:直播回放 用户在线时长上报

--- src/main/java-core/com/xueersi/parentsmeeting/modules/livevideo/business/graycontrol/LivePluginGrayConfig.java
+++ src/main/java-core/com/xueersi/parentsmeeting/modules/livevideo/business/graycontrol/LivePluginGrayConfig.java
@@ -23,7 +23,6 @@ public class LivePluginGrayConfig {
     public static final int MOUDLE_GIFT = 6;
     /** 弹幕 */
     public static final int MOUDLE_BARRAYAGE = 7;
-    /** 在线时长 */
-    public static final int MOUDLE_LECTURE_DURATION = 8;
-
+    /**用户在线时长**/
+    public static final int MOUDLE_LECTUREDURATION = 8;
 }
