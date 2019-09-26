1b896f5fd67767b5837cf196a9514f8a77e16892
chenkun2 <chenkun2@100tal.com>
2019-09-26 13:38:47 +0800

    mod:大班整合送礼物 对接BizId

--- src/main/java/com/xueersi/parentsmeeting/modules/livevideo/entity/LiveGetInfo.java
+++ src/main/java/com/xueersi/parentsmeeting/modules/livevideo/entity/LiveGetInfo.java
@@ -338,16 +338,7 @@ public class LiveGetInfo {
     private String getChatRecordUrl;
     /**扫点信息接口**/
     private String getMetadataUrl;
-    /**大班整合-bizId*/
-    private int bizId;
 
-    public int getBizId() {
-        return bizId;
-    }
-
-    public void setBizId(int bizId) {
-        this.bizId = bizId;
-    }
 
     public String getSubjectiveItem2AIUrl() {
         return subjectiveItem2AIUrl;
