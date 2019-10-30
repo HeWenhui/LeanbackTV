85f46fa6015f48ee9c18e600c69ee8cca5b2cf08
huadongliang <huadongliang@100tal.com>
2019-10-30 13:06:53 +0800

    mod:忽略字体大小
    Change-Id: I2d845b8677c8310005751b07fb9bf13b61a3cf09

--- src/main/java-core/ren/yale/android/cachewebviewlib/CacheWebView.java
+++ src/main/java-core/ren/yale/android/cachewebviewlib/CacheWebView.java
@@ -217,6 +217,8 @@ public class CacheWebView extends WebView {
 
         webSettings.setDefaultTextEncodingName("UTF-8");
 
+        webSettings.setTextZoom(100);
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
             webSettings.setAllowFileAccessFromFileURLs(true);
             webSettings.setAllowUniversalAccessFromFileURLs(true);
