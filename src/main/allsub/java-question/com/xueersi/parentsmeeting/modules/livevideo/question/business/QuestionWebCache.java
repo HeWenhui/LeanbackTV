a91faeb3d05d8e0e8dc1774f313d03b88ac7769a
linyuqiang <linyuqiang@100tal.com>
2019-11-10 16:23:20 +0800

    Mod:接麦两次弹窗修改，网页超时修改。

--- src/main/allsub/java-question/com/xueersi/parentsmeeting/modules/livevideo/question/business/QuestionWebCache.java
+++ src/main/allsub/java-question/com/xueersi/parentsmeeting/modules/livevideo/question/business/QuestionWebCache.java
@@ -15,6 +15,7 @@ import com.xueersi.parentsmeeting.modules.livevideo.core.LiveCrashReport;
 import com.xueersi.parentsmeeting.modules.livevideo.core.LiveException;
 import com.xueersi.parentsmeeting.modules.livevideo.entity.StableLogHashMap;
 import com.xueersi.parentsmeeting.modules.livevideo.util.ErrorWebViewClient;
+import com.xueersi.parentsmeeting.modules.livevideo.util.LiveMainHandler;
 import com.xueersi.parentsmeeting.modules.livevideo.util.LiveThreadPoolExecutor;
 
 import ren.yale.android.cachewebviewlib.CacheWebResourceResponse;
@@ -32,13 +33,36 @@ public class QuestionWebCache {
     private int newProgress;
     private int urlindex = 0;
     private LiveThreadPoolExecutor threadPoolExecutor = LiveThreadPoolExecutor.getInstance();
+    /** 是不是已经开始加载 */
+    private static boolean startLoad = false;
 
     public QuestionWebCache(Context context) {
         this.context = context;
     }
 
     public void startCache() {
+        logger.d("startCache:startLoad=" + startLoad);
+        if (startLoad) {
+            return;
+        }
+        startLoad = true;
+        final Handler handler = LiveMainHandler.getMainHandler();
         final CacheWebView webView = new CacheWebView(context);
+        //20秒超时，权限加载
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    logger.d("startCache:destroy1");
+                    startLoad = false;
+                    webView.stopLoading();
+                    webView.destroy();
+                } catch (Exception e) {
+                    LiveCrashReport.postCatchedException(TAG, e);
+                }
+            }
+        };
+        handler.postDelayed(runnable, 20000);
         webView.setWebViewClient(new MyWebViewClient());
         final String examUrl = "https://live.xueersi.com/science/Live/getMultiTestPaper?liveId=119740&testId=365160-1&stuId=-111&stuName=test@talwx.com&stuCouId=12345654&isArts=0&nonce=45645dasf&isTowall=0";
         webView.loadUrl(examUrl);
@@ -73,9 +97,19 @@ public class QuestionWebCache {
             }
         });
         webView.setWebChromeClient(new WebChromeClient() {
+            boolean hasdestroy = false;
+
             @Override
             public void onProgressChanged(WebView webView, int i) {
                 newProgress = i;
+                if (i == 100) {
+                    startLoad = false;
+                    if (hasdestroy) {
+                        return;
+                    }
+                    hasdestroy = true;
+                    handler.removeCallbacks(runnable);
+                }
             }
         });
     }
