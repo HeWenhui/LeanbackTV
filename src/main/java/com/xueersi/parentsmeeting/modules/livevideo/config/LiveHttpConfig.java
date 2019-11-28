7f5e2b2483983d1bf34348940c142f4d38c36f76
WangQing <wangqing11@100tal.com>
2019-11-28 15:05:21 +0800

    1. 批量下载功能优化_2。

--- src/main/java/com/xueersi/parentsmeeting/modules/livevideo/config/LiveHttpConfig.java
+++ src/main/java/com/xueersi/parentsmeeting/modules/livevideo/config/LiveHttpConfig.java
@@ -92,10 +92,7 @@ public class LiveHttpConfig {
      * TODO 待定？
      * 2019年11月22日 课件预加载加载地址
      */
-//    public static String URL_LIVE_GET_ALL_SUBJECT = "https://studentlive.xueersi.com/v1/student/classroom/preLoad/courseware/";
-//    public static String URL_LIVE_GET_ALL_SUBJECT = "http://studentlive.xueersi.com/v1/student/classroom/preLoad/courseware/";
-    public static String URL_LIVE_GET_ALL_SUBJECT = "http://studentlive.xueersi.com/v1/student/classroom/preload/courseware";
-//    public static String URL_LIVE_GET_ALL_SUBJECT = "http://10.90.101.199:19001/v1/student/classroom/preload/courseware";
+    public static String URL_LIVE_GET_ALL_SUBJECT = LiveIntegratedCfg.HTTP_HOST + "/v1/student/classroom/preload/courseware";
 
 
     public static final String URL_GOLD_MICROPHONE_TO_AI = LiveVideoConfig.HTTP_PRIMARY_CHINESE_HOST + "/LiveCourse/isGoldMicrophoneToAi";
