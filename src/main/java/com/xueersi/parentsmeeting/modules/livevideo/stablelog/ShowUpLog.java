a96575d3bfe4d2ada5efc1151be673632ab01ab4
wangyongchao <wangyongchao@100tal.com>
2019-12-03 18:10:33 +0800

    add:上台日志完善
    Change-Id: I6b066b47461db5f9a8ee3af54bff2cb779e7f43b

---
+++ src/main/java/com/xueersi/parentsmeeting/modules/livevideo/stablelog/ShowUpLog.java
@@ -0,0 +1,54 @@
+package com.xueersi.parentsmeeting.modules.livevideo.stablelog;
+
+import com.xueersi.parentsmeeting.modules.livevideo.business.LiveAndBackDebug;
+import com.xueersi.parentsmeeting.modules.livevideo.entity.StableLogHashMap;
+
+/**
+ * 上台日志
+ */
+public class ShowUpLog {
+    private static final String EVENT_ID = "cometostage";
+
+    /**
+     * 学生端收到发题指令时间点
+     */
+    public static void reciveQuestion(LiveAndBackDebug liveAndBackDebug, boolean isplayback) {
+        StableLogHashMap logHashMap = new StableLogHashMap("receiveComeToStage");
+        logHashMap.put("isplayback", isplayback ? "1" : "0");
+        liveAndBackDebug.umsAgentDebugInter(EVENT_ID, logHashMap.getData());
+    }
+
+    /**
+     * 是否成功加载课件
+     */
+    public static void loadCoursewareStatus(LiveAndBackDebug liveAndBackDebug, boolean isplayback
+            , boolean success) {
+        StableLogHashMap logHashMap = new StableLogHashMap("loadingsucceedComeToStage");
+        logHashMap.put("loadingsucceed", success ? "1" : "0");
+        logHashMap.put("isplayback", isplayback ? "1" : "0");
+        liveAndBackDebug.umsAgentDebugPv(EVENT_ID, logHashMap.getData());
+    }
+
+    /**
+     * 测评大于 0 分的单词文本+分数（多条）
+     */
+    public static void voiceTestResult(LiveAndBackDebug liveAndBackDebug, String word
+            , int score, boolean isplayback) {
+        StableLogHashMap logHashMap = new StableLogHashMap("submitComeToStage");
+        logHashMap.put("word", word);
+        logHashMap.put("score", String.valueOf(score));
+        logHashMap.put("isplayback", isplayback ? "1" : "0");
+        liveAndBackDebug.umsAgentDebugInter(EVENT_ID, logHashMap.getData());
+    }
+
+    /**
+     * 结果页弹出时间
+     */
+    public static void popResultPage(LiveAndBackDebug liveAndBackDebug, boolean isplayback) {
+        StableLogHashMap logHashMap = new StableLogHashMap("displayResults");
+        logHashMap.put("isplayback", isplayback ? "1" : "0");
+        liveAndBackDebug.umsAgentDebugPv(EVENT_ID, logHashMap.getData());
+    }
+
+
+}
