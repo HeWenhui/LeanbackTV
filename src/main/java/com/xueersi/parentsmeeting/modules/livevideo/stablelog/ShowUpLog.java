ee8f13a7c26896cf7108d8f41e37bea0ad867114
wangyongchao <wangyongchao@100tal.com>
2019-12-06 10:23:29 +0800

    add:上台埋点增加字段
    Change-Id: Ic6231bc048cd4e147d27bfa2819d371fa10a0d37

--- src/main/java/com/xueersi/parentsmeeting/modules/livevideo/stablelog/ShowUpLog.java
+++ src/main/java/com/xueersi/parentsmeeting/modules/livevideo/stablelog/ShowUpLog.java
@@ -12,9 +12,12 @@ public class ShowUpLog {
     /**
      * 学生端收到发题指令时间点
      */
-    public static void reciveQuestion(LiveAndBackDebug liveAndBackDebug, boolean isplayback) {
+    public static void reciveQuestion(LiveAndBackDebug liveAndBackDebug, boolean isplayback,
+                                      String interactionid) {
         StableLogHashMap logHashMap = new StableLogHashMap("receiveComeToStage");
         logHashMap.put("isplayback", isplayback ? "1" : "0");
+        logHashMap.put("interactionid", interactionid);
+        logHashMap.put("sno", "1");
         liveAndBackDebug.umsAgentDebugInter(EVENT_ID, logHashMap.getData());
     }
 
@@ -22,10 +25,12 @@ public class ShowUpLog {
      * 是否成功加载课件
      */
     public static void loadCoursewareStatus(LiveAndBackDebug liveAndBackDebug, boolean isplayback
-            , boolean success) {
+            , boolean success, String interactionid) {
         StableLogHashMap logHashMap = new StableLogHashMap("loadingsucceedComeToStage");
         logHashMap.put("loadingsucceed", success ? "1" : "0");
         logHashMap.put("isplayback", isplayback ? "1" : "0");
+        logHashMap.put("interactionid", interactionid);
+        logHashMap.put("sno", "2");
         liveAndBackDebug.umsAgentDebugPv(EVENT_ID, logHashMap.getData());
     }
 
@@ -33,20 +38,25 @@ public class ShowUpLog {
      * 测评大于 0 分的单词文本+分数（多条）
      */
     public static void voiceTestResult(LiveAndBackDebug liveAndBackDebug, String word
-            , int score, boolean isplayback) {
+            , int score, boolean isplayback, String interactionid) {
         StableLogHashMap logHashMap = new StableLogHashMap("submitComeToStage");
         logHashMap.put("word", word);
         logHashMap.put("score", String.valueOf(score));
         logHashMap.put("isplayback", isplayback ? "1" : "0");
+        logHashMap.put("interactionid", interactionid);
+        logHashMap.put("sno", "3");
         liveAndBackDebug.umsAgentDebugInter(EVENT_ID, logHashMap.getData());
     }
 
     /**
      * 结果页弹出时间
      */
-    public static void popResultPage(LiveAndBackDebug liveAndBackDebug, boolean isplayback) {
+    public static void popResultPage(LiveAndBackDebug liveAndBackDebug, boolean isplayback,
+                                     String interactionid) {
         StableLogHashMap logHashMap = new StableLogHashMap("displayResults");
         logHashMap.put("isplayback", isplayback ? "1" : "0");
+        logHashMap.put("interactionid", interactionid);
+        logHashMap.put("sno", "4");
         liveAndBackDebug.umsAgentDebugPv(EVENT_ID, logHashMap.getData());
     }
 
