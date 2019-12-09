166ffe4b6ef29589914bee3591c4aab60e4ad3a8
wangde <wangde@100tal.com>
2019-12-09 20:42:57 +0800

    mod:红包接口类变化

--- src/main/java/com/xueersi/parentsmeeting/modules/livevideo/business/lightlive/bll/LightLiveRedPackageBll.java
+++ src/main/java/com/xueersi/parentsmeeting/modules/livevideo/business/lightlive/bll/LightLiveRedPackageBll.java
@@ -106,6 +106,11 @@ public class LightLiveRedPackageBll implements RedPackageAction, Handler.Callbac
         });
     }
 
+    @Override
+    public void onRemoveRedPackage() {
+
+    }
+
     private void onGetPackageFailure(int operateId) {
     }
 
