696601c0c1f32e8458e99f2cca49f261581bf070
zhangyuansun <zhangyuansun@100tal.com>
2020-01-03 16:00:39 +0800

    add:增加userId校验

--- src/main/java-core/com/xueersi/parentsmeeting/modules/livevideo/config/LiveCoreConfig.java
+++ src/main/java-core/com/xueersi/parentsmeeting/modules/livevideo/config/LiveCoreConfig.java
@@ -1,9 +1,12 @@
 package com.xueersi.parentsmeeting.modules.livevideo.config;
 
+import com.xueersi.common.business.UserBll;
+
 public class LiveCoreConfig {
     /** 获取假流信息   */
     public static final String URL_LIVE_GROUP_CLASS_VIRTUAL = LiveIntegratedCfg.HTTP_HOST+"/v1/student/getVirtualStuData";
 
-    public static final String SP_LIVE_GROUP_CLASS_VIRTUAL_INFO = "sp_live_group_class_virtual_info";
+    public static final String SP_LIVE_GROUP_CLASS_VIRTUAL_INFO =
+            "sp_live_group_class_virtual_info" + UserBll.getInstance().getMyUserInfoEntity().getStuId();
 
 }
