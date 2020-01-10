3fe64fe79113b2066719a10db3c8e4fc739d0d5e
wuqingfeng <943000144@qq.com>
2020-01-10 11:18:43 +0800

    ai主观互动题添加

--- src/main/java-core/com/xueersi/parentsmeeting/modules/livevideo/business/graycontrol/LivePluginGrayConfig.java
+++ src/main/java-core/com/xueersi/parentsmeeting/modules/livevideo/business/graycontrol/LivePluginGrayConfig.java
@@ -47,4 +47,6 @@ public class LivePluginGrayConfig {
     public static final int MODULE_TEAM_PK = 70;
     /** roleplay */
 //    public static final int MODULE_ROLE_PLAY = 90;
+    /** ai主观题课件互动题 **/
+    public static final int MOUDLE_AI_COURSEWARE = 127;
 }
